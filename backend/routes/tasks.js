97f1c244aa28ab63b2cdfe07cd068f83c46c4850
Chen Tobiana <chentobiana@gmail.com>
2024-09-03 22:19:47 +0300

    First UI with add tasks and filter

--- backend/routes/tasks.js
+++ backend/routes/tasks.js
@@ -30,4 +30,25 @@ router.post('/', async (req, res) => {
   }
 });
 
+// Update a task
+router.put('/:id', async (req, res) => {
+  try {
+    const updatedTask = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    res.status(200).json(updatedTask);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+// Delete a task
+router.delete('/:id', async (req, res) => {
+  try {
+    await Task.findByIdAndDelete(req.params.id);
+    res.status(200).json({ message: 'Task deleted successfully.' });
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+
 module.exports = router;
